2bbaa74653b318be457c455c19c78edd12469752
pappou <bruch@online.de>
2020-05-12 21:45:07 +0200

    extended lists, works!

--- gstreamcam.py
+++ gstreamcam.py
@@ -16,7 +16,7 @@ def createsdp(hostname, streams):
     sdp.append('t=0 0')
     sdp.append('s=GST2SDP')
 
-    streamnumber = 1
+    streamnumber = 2
 
     # add individual streams to SDP
     for stream in streams:
@@ -46,21 +46,73 @@ def main(arguments):
     
     gstreamer = 'gst-launch-1.0.exe' if 'win' in sys.platform else 'gst-launch-1.0'
     hostname = arguments.hostname
-    v_inputs = {"webcam" : "v4l2src",
-                "decklink" : "decklinkvideosrc device-number=%d do-timestamp=true" % arguments.device,
-                "test" : "videotestsrc",
-                 "original" : "ksvideosrc device_index=%d" % arguments.device }
-    v_src = v_inputs[arguments.v_input]
+
+
+    # define audio input in dict like {"argument" : (b"audioinput", "audiopipeline")}
+    a_pipeline = ["!", "queue", "!", "audioconvert", "!", "audioresample", "!", "queue", "!", "jackaudiosink", "connect=0", "client-name=Video%d" % arguments.device]
+    a_inputs = {
+                "webcam" : ('',''),
+                "decklink" : (['decklinkaudiosrc', 'device-number=%d' % arguments.device, 'do-timestamp=true'], "%s" % a_pipeline),
+                "test" : (['audiotestsrc', 'is-live=1', 'do-timestamp=true', 'wave=1'], a_pipeline),
+                "original" : ("", "")
+                }
+    a_src = a_inputs[arguments.v_input][0]
+    # print(a_src)
+    a_pip = a_inputs[arguments.v_input][1]
+
     encoders = {'h264' : (b'GstRtpH264Pay' , 'x264enc', 'rtph264pay'),
                 'vp8' : (b'GstRtpVP8Pay', 'vp8enc', 'rtpvp8pay'),
                 'openh264' : (b'GstRtpH264Pay', 'openh264enc', 'rtph264pay')}
     rtppay = encoders[arguments.codec][0]
     port = arguments.port
-    arglist = [gstreamer, "-v", v_src, "!", "videoconvert", "!", "videoscale", "!", "video/x-raw,width=1920,height=1080", "!",
-                    encoders[arguments.codec][1],  "!", encoders[arguments.codec][2], "!", "udpsink", "host=%s" % hostname, "port=%d" % port]
+    v_inputs = {
+                "webcam" : ["v4l2src"],
+                "decklink" : ["decklinkvideosrc", "device-number=%d" % arguments.device, "do-timestamp=true"],
+                "test" : ["videotestsrc"],
+                "original" : ["ksvideosrc", "device_index=%d" % arguments.device]
+                }
+    v_src = v_inputs[arguments.v_input]
+    v_pip = ["!", "videoconvert", "!",
+                "videoscale", "!",
+                "video/x-raw,width=1920,height=1080", "!",
+                encoders[arguments.codec][1],  "!",
+                encoders[arguments.codec][2], "!",
+                "udpsink",
+                "host=%s" % hostname,
+                "port=%d" % port
+                ]
+    arglist = [gstreamer, "-v"]
+    arglist.extend(a_src)
+    arglist.extend(a_pip)
+    arglist.extend(v_src)
+    arglist.extend(v_pip)
+    print("Argliste:")
+    print(arglist)
+    arglist_old = [
+                gstreamer, "-v",
+                a_src, "!",
+                "audioconvert" , "!",
+                "audioresample", "!",
+                "jackaudiosink",
+                # "%s" % a_pip,
+                "%s" % v_src, "!",
+                "videoconvert", "!",
+                "videoscale", "!",
+                "video/x-raw,width=1920,height=1080", "!",
+                encoders[arguments.codec][1],  "!",
+                encoders[arguments.codec][2], "!",
+                "udpsink",
+                "host=%s" % hostname,
+                "port=%d" % port
+                ]
+    print(arglist)
     if arguments.debug:
         print("Calling gstreamer:\n"," ".join(arglist))
+    commandstring = ' '.join(arglist)
+    print()
+    print(commandstring)
     process = Popen(arglist, stdout=PIPE)
+    # process = Popen(commandstring, shell=True)
 
     def signal_handler(signal, frame):
         process.kill()
@@ -97,7 +149,7 @@ if __name__ == "__main__":
     parser.add_argument("--port", "-p", help="port (defaults to 5000)", type=int, default=5000)
     parser.add_argument("--codec", help="chooses encoder (defaults to openh264)", choices=['vp8', 'h264', 'openh264'], default='openh264')
     parser.add_argument("--device", help="Device id (defaults to 0)", type=int, default=0)
-    parser.add_argument("--v_input", help="", choices=['webcam', 'decklinkvideosrc', 'test', 'original'], default="test")
+    parser.add_argument("--v_input", help="", choices=['webcam', 'decklink', 'test', 'original'], default="test")
 
     args = parser.parse_args()
 
