19332f42eb50ca64470294d7e3a77d7de6ae6081
pappou99 <28729133+pappou99@users.noreply.github.com>
2020-05-14 15:53:47 +0200

    Works, but only 2 Channels Audio

--- gstreamcam.py
+++ gstreamcam.py
@@ -9,7 +9,7 @@ import sys
 
 random.seed(None)
 
-def createsdp(hostname, streams):
+def createsdp(hostname, streams, device):
     params2ignore = set(['encoding-name', 'timestamp-offset', 'payload', 'clock-rate', 'media', 'port'])
     sdp = ['v=0']
     sdp.append('o=- %d %d IN IP4 %s' % (random.randrange(4294967295), 2, hostname))
@@ -38,7 +38,7 @@ def createsdp(hostname, streams):
         streamnumber += 1
 
     # save sdp
-    with open('session.sdp','w') as f:
+    with open('Video%d.sdp' % device,'w') as f:
         f.write('\r\n'.join(sdp))
 
 
@@ -46,13 +46,21 @@ def main(arguments):
     
     gstreamer = 'gst-launch-1.0.exe' if 'win' in sys.platform else 'gst-launch-1.0'
     hostname = arguments.hostname
+    device = arguments.device
+    devicename = device + 1
 
 
     # define audio input in dict like {"argument" : (b"audioinput", "audiopipeline")}
-    a_pipeline = ['!', 'tee', 'name=audio', "!", "queue", "!", "audioconvert", "!", "audioresample", "!", "queue", "!", "jackaudiosink", "connect=0", "client-name=Video%d" % arguments.device]
+    a_pipeline = ['!', 'tee', 'name=audio'
+                    , "!", "queue", "!", "audioconvert", "!", "audioresample", "!", "queue", "!", "jackaudiosink", "connect=0", "client-name=Video%d" % devicename
+                    # , "audio.", "!", "fakesink"
+                    # ,"audio.", "!", "audioconvert", "!",
+                    # , 'audio.', '!', 'voaacenc', '!', 'mux.' # geht gut, aber nur 2 Kanäle Audio
+                    , "audio.", "!", "opusenc", "!", "mux." # geht gut, aber nur 2 Kanäle Audio
+                ]
     a_inputs = {
                 "webcam" : ('',''),
-                "decklink" : (['decklinkaudiosrc', 'device-number=%d' % arguments.device, 'connection=1', 'channels=8', 'do-timestamp=true'], a_pipeline),
+                "decklink" : (['decklinkaudiosrc', 'device-number=%d' % device, 'connection=1', 'channels=2', 'do-timestamp=true'], a_pipeline),
                 "test" : (['audiotestsrc', 'is-live=1', 'do-timestamp=true', 'wave=1', '!', 'audio/x-raw,channels=8'], a_pipeline),
                 "original" : ("", "")
                 }
@@ -61,19 +69,24 @@ def main(arguments):
     a_pip = a_inputs[arguments.v_input][1]
 
     encoders = {
-                'h264nosound' : (b'GstRtpH264Pay' , 'x264enc', 'rtph264pay'),
+                'h264' : (b'GstRTPMP2TPay' , 'x264enc', 'rtpmp2tpay'),
+                'h264old' : (b'GstRtpH264Pay' , 'x264enc', 'rtph264pay'),
                 'vp8' : (b'GstRtpVP8Pay', 'vp8enc', 'rtpvp8pay'),
                 'openh264-nosound' : (b'GstRtpH264Pay', 'openh264enc', 'rtph264pay'),
+                'mp4' : (b'GstRTPMP2TPay', 'avenc_mpeg4', 'rtpmp2tpay'),
                 'openh264' : (b'GstRtpH264Pay', 'openh264enc', 'rtph264pay')
                 }
-    muxer = ['mpegtsmux', 'alignment=7', 'name=mux', '!']
+    muxer = [
+            'mpegtsmux', 'alignment=7', 'name=mux', '!'
+            # 'mp4mux', 'name=mux', '!'
+            ]
     rtppay = encoders[arguments.codec][0]
     port = arguments.port
     v_inputs = {
                 "webcam" : ["v4l2src"],
-                "decklink" : ["decklinkvideosrc", "device-number=%d" % arguments.device, "do-timestamp=true"],
+                "decklink" : ["decklinkvideosrc", "device-number=%d" % device, "do-timestamp=true"],
                 "test" : ["videotestsrc"],
-                "original" : ["ksvideosrc", "device_index=%d" % arguments.device]
+                "original" : ["ksvideosrc", "device_index=%d" % device]
                 }
     v_src = v_inputs[arguments.v_input]
     v_enc = [encoders[arguments.codec][1], '!']
@@ -135,7 +148,8 @@ def main(arguments):
             pattern = p.search(line)
             if pattern and not patternGenerated:
                 parameters = re.findall(rb'(([\w-]+)=(?:\(\w+\))?(?:(\w+)|(?:"([^"]+)")))', pattern.groups()[0])
-                #print(parameters)
+                print('Parameter:')
+                print(parameters)
                 parammap = defaultdict(str)
                 for (_, param, value, value2) in parameters:
                     parammap[param.decode('ascii')] = value.decode('ascii') if value else value2.decode('ascii')
@@ -144,7 +158,7 @@ def main(arguments):
                 if len(parammap) > 0:
                     patternGenerated = True
                     if arguments.sdp:
-                        createsdp(hostname, [parammap])
+                        createsdp(hostname, [parammap], devicename)
                     for param,value in parammap.items():
                         print("%s = %s" % (param, value))
     finally:
@@ -156,9 +170,9 @@ if __name__ == "__main__":
     parser.add_argument("--sdp", help="generates SDP file for the stream (defaults to false)", action="store_true")
     parser.add_argument("--debug", help="shows command line in use to call gstreamer", action="store_true")
     parser.add_argument("--port", "-p", help="port (defaults to 5000)", type=int, default=5000)
-    parser.add_argument("--codec", help="chooses encoder (defaults to openh264)", choices=['vp8', 'h264', 'openh264'], default='openh264')
+    parser.add_argument("--codec", help="chooses encoder (defaults to openh264)", choices=['vp8', 'h264', 'openh264', 'mp4'], default='mp4')
     parser.add_argument("--device", help="Device id (defaults to 0)", type=int, default=0)
-    parser.add_argument("--v_input", help="", choices=['webcam', 'decklink', 'test', 'original'], default="test")
+    parser.add_argument("--v_input", help="", choices=['webcam', 'decklink', 'test', 'original'], default="decklink")
 
     args = parser.parse_args()
 
@@ -166,3 +180,8 @@ if __name__ == "__main__":
     print("Using hostname %s using device %d" % (args.hostname, args.device))
 
     main(args)
+#
+#mögliche andere muxer:
+# avmux_psp a: mpeg4                v: x-h264 -> quicktime
+# mp4mux    a: mpeg1,4; ac3; opus   v: mpeg4; divx; x-h264; x-h265; x-mp4-part; x-av1
+# 
