cd26749c4b7175c32889bd2abe8b0c202a9d0270
pappou99 <no@me.int>
2020-05-20 14:37:14 +0200

    expanded to audio encoder selection

--- gstreamcam.py
+++ gstreamcam.py
@@ -51,26 +51,38 @@ def main(arguments):
 
 
     # define audio input in dict like {"argument" : (b"audioinput", "audiopipeline")}
-    a_pipeline = ['!', 'tee', 'name=audio'
-                    , "!", "queue", "!", "audioconvert", "!", "audioresample", "!", "queue", "!", "jackaudiosink", "connect=0", "client-name=Video%d" % devicename
+    a_pipeline = [
+                    'audio.', "!", "queue", "!", "audioconvert", "!", "audioresample", "!", "queue", "!", "jackaudiosink", "connect=0", "client-name=Video%d" % devicename
                     # , "audio.", "!", "fakesink"
                     # ,"audio.", "!", "audioconvert", "!",
                     # , 'audio.', '!', 'voaacenc', '!', 'mux.' # geht gut, aber nur 2 Kanäle Audio
                     , "audio.", "!", 'deinterleave', 'name=d'
-                    , 'interleave', 'channel-positions-from-input=true', 'name=i', '!', 'audioconvert', '!', "opusenc", "!", 'mux.'
+                    , 'interleave', 'channel-positions-from-input=true', 'name=i', '!', 'audioconvert', '!', 'a_enc.'
+                    # , "opusenc", 'name=a_enc', "!", 'mux.'
                     , 'd.src_0', '!', 'i.sink_0'
                     # , 'd.src_1', '!', 'i.sink_1'
                     # , 'd.src_0', '!', 'i.sink_0', 'interleave', 'name=i', '!', "opusenc", "!", "mux." # geht gut, aber nur 2 Kanäle Audio
                 ]
-    a_inputs = {
-                "webcam" : ('',''),
-                "decklink" : (['decklinkaudiosrc', 'device-number=%d' % device, 'connection=1', 'channels=8', 'do-timestamp=true'], a_pipeline),
-                "test" : (['audiotestsrc', 'is-live=1', 'do-timestamp=true', '!', 'audio/x-raw,channels=8'], a_pipeline),
-                "original" : ("", "")
+
+    a_encoder = {
+                    "opus" : ["opusenc", ],
                 }
-    a_src = a_inputs[arguments.v_input][0]
+
+    a_enc_pip = a_encoder[arguments.audio]
+    a_enc_pip.extend(['name=a_enc', "!", 'mux.'])
+    print(a_enc_pip)
+
+    a_inputs = {
+                "webcam" : '',
+                "decklink" : ['decklinkaudiosrc', 'device-number=%d' % device, 'connection=1', 'channels=8', 'do-timestamp=true'],
+                "test" : ['audiotestsrc', 'is-live=1', 'do-timestamp=true', '!', 'audio/x-raw,channels=8'],
+                "original" : ''
+                }
+    a_src = a_inputs[arguments.input]
+    a_pip = ['!', 'tee', 'name=audio']
     # print(a_src)
-    a_pip = a_inputs[arguments.v_input][1]
+    a_pip.extend(a_pipeline)
+    a_pip.extend(a_enc_pip)
 
     encoders = {
                 'h264' : (b'GstRTPMP2TPay' , 'x264enc', 'rtpmp2tpay'),
@@ -86,13 +98,13 @@ def main(arguments):
             ]
     rtppay = encoders[arguments.codec][0]
     port = arguments.port
-    v_inputs = {
+    inputs = {
                 "webcam" : ["v4l2src"],
                 "decklink" : ["decklinkvideosrc", "device-number=%d" % device, "do-timestamp=true"],
                 "test" : ["videotestsrc"],
                 "original" : ["ksvideosrc", "device_index=%d" % device]
                 }
-    v_src = v_inputs[arguments.v_input]
+    v_src = inputs[arguments.input]
     v_enc = [encoders[arguments.codec][1], '!']
     v_pay = [encoders[arguments.codec][2], '!']
     v_sink =    ["udpsink",
@@ -176,7 +188,8 @@ if __name__ == "__main__":
     parser.add_argument("--port", "-p", help="port (defaults to 5000)", type=int, default=5000)
     parser.add_argument("--codec", help="chooses encoder (defaults to openh264)", choices=['vp8', 'h264', 'openh264', 'mp4'], default='mp4')
     parser.add_argument("--device", help="Device id (defaults to 0)", type=int, default=0)
-    parser.add_argument("--v_input", help="", choices=['webcam', 'decklink', 'test', 'original'], default="decklink")
+    parser.add_argument("--input", help="", choices=['webcam', 'decklink', 'test', 'original'], default="decklink")
+    parser.add_argument("--audio", help="Audiocodec to choose for the stream", choices=['opus',], default='opus')
 
     args = parser.parse_args()
 
