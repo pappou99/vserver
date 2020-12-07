59364cb2b7fbd6962316a435579eab3eb53e4e9b
pappou <bruch@online.de>
2020-12-07 15:09:07 +0100

    switch all develepmont related parameters depending to devel switch Fixes #5

--- vServer_settings.py
+++ vServer_settings.py
@@ -23,7 +23,7 @@ from vServer_secrets import Secrets
 
 class Settings:
     debug = True
-    homework = True
+    development = True #change to true, if you dont have the required hardware (audio- videotestsrc)
     instant_play = False
     interactive = False
 
@@ -32,16 +32,16 @@ class Settings:
 
     # stream_ip = '239.230.225.255'# multicast Address
     stream_ip = '10.82.109.41' #rtmp server address
-    if homework == True: stream_ip = '10.19.77.42' #rtmp server address
+    if development == True: stream_ip = '10.19.77.42' #rtmp server address
     startport = 5001
     speed_preset = 3
     amplification = 4
 
-    # video_in_name = 'Decklink-Card' ## must be exactly written like in vServer.choice.py class PossibleInputs
-    # audio_in_name = 'Decklink-Card' ## must be exactly written like in vServer.choice.py class PossibleInputs
-    video_in_name = 'Test picture generator' ## must be exactly written like in vServer.choice.py class PossibleInputs
+    video_in_name = 'Decklink-Card' ## must be exactly written like in vServer.choice.py class PossibleInputs
+    if development: video_in_name = 'Test picture generator' ## must be exactly written like in vServer.choice.py class PossibleInputs
+    audio_in_name = 'Decklink-Card' ## must be exactly written like in vServer.choice.py class PossibleInputs
     # video_in_name = 'Webcam' ## must be exactly written like in vServer.choice.py class PossibleInputs
-    audio_in_name = 'Test sound generator' ## must be exactly written like in vServer.choice.py class PossibleInputs
+    if development: audio_in_name = 'Test sound generator' ## must be exactly written like in vServer.choice.py class PossibleInputs
     videowidth = '1280'
     videoheight = '720'
     
@@ -57,7 +57,8 @@ class Settings:
     num_streams = 2
     
     audio_channels_from_sdi = 8
-    audio_channels_to_madi = 1#should be 8 for real life
+    audio_channels_to_madi = 8
+    if development: audio_channels_to_madi = 1
     audio_channels_to_stream = 1
     default_audio_to_stream = 1
     
@@ -66,7 +67,7 @@ class Settings:
     stream = ''
 
     mqtt_server = '10.82.209.45'
-    if homework == True: mqtt_server = 'localhost'
+    if development == True: mqtt_server = 'localhost'
     mqtt_port = 1883
     mqtt_topic = ['gvg-grp', maschinename, 'video']
     mqtt_user = Secrets.mqtt_user
--- vserver/jackconnect.py
+++ vserver/jackconnect.py
@@ -59,7 +59,7 @@ class Jacking(): #threading.Thread
             capture = self.client.get_ports(name_pattern='%s' % self.clientname, is_audio=True, is_output=True, is_physical=False)
             playback = self.client.get_ports(is_physical=True, is_input=True)
             print('Jack: Playbackports: %s' % playback)
-            if Settings.homework == False:
+            if Settings.development == False:
                 # delete first two elements of the madi-card (analogue jack outputs, we will never connect!)
                 print('JACK: Removing the first two ports')
                 del playback[1]
